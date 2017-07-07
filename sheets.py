9edd0cbb6f86268809f4343c198f43e9cf857ade
I-Dont-Remember <kpquinn2@wisc.edu>
2017-07-06 23:59:57 -0500

    Streamlines sms object usage
    SMS is now an easy to pass object.  Started early stages of
    switching based on sender and parsing text.

--- sheets.py
+++ sheets.py
@@ -2,7 +2,10 @@
 import gspread
 import logging
 from oauth2client.service_account import ServiceAccountCredentials
+from server import SMS
 
+# NEED BETTER WAY FOR DEFINE TYPE THINGS
+KEVIN = '17153387410'
 ####### HOW DOE WE LOG STUFF THAT ISNT PRINT############
 # log = logging.getLogger('log')
 # logging.basicConfig()
@@ -25,17 +28,36 @@ from oauth2client.service_account import ServiceAccountCredentials
 # 
 # # sheet.update_cell(marker.row +1, marker.col, marker.value)
 
-def authorize():
+def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
     client = gspread.authorize(creds)
-    return client
-
-def update(message):
-    client = authorize()
     sheet = client.open("Copy of Summer Fun Spending").sheet1
-    marker = sheet.find("--PREV--")
+    return sheet 
+
+def split_message(message):
+    # [ Date, Description, Price ]
+    # If has date listed use, otherwise get current date
+    # Separate by commas in text for ease of use?
+    msg_parts = []
+    
+def input_message(sms, marker, sheet):
     print("Found marker at R%sC%s" % (marker.row, marker.col))
-    sheet.update_cell(marker.row + 2, marker.col, message)
+    sheet.update_cell(marker.row+1, marker.col, marker.value)
+    # Split up message into the correct pieces and input
+    msg_parts = sms.text.split(',')
+    # if correct num chunks, 3 with date or 2 and use current date
+    for i in range(0,3):
+        sheet.update_cell(marker.row, marker.col + i, msg_parts[i])
+    # sheet.update_cell(marker.row, marker.col, sms.text)
 
+######### Main #########################
 
+def update(sms):
+    sheet = get_sheet()
+    # Error check
+    marker = sheet.find("--PREV--")
+    if (sms.from_num == KEVIN):
+        input_message(sms, marker, sheet)
+    else:
+        print sms.from_num
