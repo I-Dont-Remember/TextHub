1860482058cc350823dfef3cc29f1d19097203e3
I-Dont-Remember <kpquinn2@wisc.edu>
2017-07-17 21:57:23 -0500

    Revamped and streamlined
    Logging beginnings, start of cleaner config usage, and refactored
    several pieces to be more scalable as we progress.

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env  python
 import plivo,plivoxml
 import sheets
-from flask import Flask, send_file, request
+from flask import Flask,request
 from collections import namedtuple
+import ConfigParser
+import config
 
 # Depending on how this shapes up, a class for each person and their
-#    number might be nice
+#    number might be nice -- or dictionary
 
 class SMS(object):
     from_num = ""
@@ -17,30 +19,43 @@ class SMS(object):
         self.to_num = to_num
         self.text = text
 
-# Refresh/ Get Oauth token in the API file, 
+# Refresh/ Get Oauth token in the API file,
 #   server gets called way wayyyy to infrequently
 app = Flask(__name__)
-
 @app.route("/hello")
 def hello():
     return "Hello World from Flask"
 
 # Find way to run multiple people on one phone number
 @app.route("/sms", methods=['GET', 'POST'])
-def sms():
-    print "in sms"
+def message():
     from_num = request.values.get('From')
     to_num = request.values.get('To')
     text = request.values.get('Text')
     full_sms = SMS(from_num, to_num, text)
     print "Message received - From: %s, To: %s, Text: %s" % (from_num, to_num, text)
-    sheets.update(full_sms)
+    sheets.handle_sms(full_sms)
+    # Should we be returning something other than 200?
     return 'Message received', 200
 
 @app.route("/")
 def main():
-    return "You're at Home"
+    return "Uh-oh, I don't think you're supposed to be here."
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', debug=True, port=80)
-    app.run(port=1337)
+#    cp = ConfigParser.SafeConfigParser()
+#    cp.read('sms.ini')
+#    section = 'INFO'
+#    PORT = cp.get(section, 'port')
+#    SECRET = cp.get(section, 'client_file')
+#    SHEET_NAME = cp.get(section, 'sheet_name')
+#    print('sheet is "%s"' % SHEET_NAME)
+#    print(SECRET)
+#    PEOPLE = {}
+#    if cp.has_section('PEOPLE'):
+#        options = cp.options('PEOPLE')
+#        for person in options:
+#            PEOPLE[person] = cp.get('PEOPLE', person)
+    # if confiig parsing fails, exit
+    app.run(port=config.port)
--- sheets.py
+++ sheets.py
@@ -2,62 +2,62 @@
 import gspread
 import logging
 from oauth2client.service_account import ServiceAccountCredentials
-from server import SMS
+import server
+import config
+import datetime
 
-# NEED BETTER WAY FOR DEFINE TYPE THINGS
-KEVIN = '17153387410'
-####### HOW DOE WE LOG STUFF THAT ISNT PRINT############
-# log = logging.getLogger('log')
-# logging.basicConfig()
-# log.setLevel(logging.DEBUG)
+log = logging.getLogger('log')
+logging.basicConfig(level=logging.DEBUG)
 # # When changing sheets, need to make sure to go and share this client.json email with the sheet you want to work on
-# scope = ['https://spreadsheets.google.com/feeds']
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
-# client = gspread.authorize(creds)
-# 
-# sheet = client.open("Copy of Summer Fun Spending").sheet1
-# 
-# # Leave a marker term and use cell find instead of trying anything fancy?
-# x# marker = sheet.find("--PREV--")
-# # For every API call, if it fails exit gracefully with log messages
-# print("Found marker at R%sC%s" % (marker.row, marker.col))
-# 
-# # Move marker down to the next row
-# 
-# # Fill in prev row values
-# 
-# # sheet.update_cell(marker.row +1, marker.col, marker.value)
 
-def get_sheet():
+def get_sheet(sheet_name):
     scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(config.client_file, scope)
     client = gspread.authorize(creds)
-    sheet = client.open("Copy of Summer Fun Spending").sheet1
-    return sheet 
+    try:
+        sheet = client.open(sheet_name).sheet1
+    except  gspread.exceptions.SpreadsheetNotFound:
+        log.debug('Sheet "%s" not found' % sheet_name)
+        return None
+    else:
+        return sheet
 
-def split_message(message):
-    # [ Date, Description, Price ]
-    # If has date listed use, otherwise get current date
-    # Separate by commas in text for ease of use?
-    msg_parts = []
-    
 def input_message(sms, marker, sheet):
     print("Found marker at R%sC%s" % (marker.row, marker.col))
-    sheet.update_cell(marker.row+1, marker.col, marker.value)
     # Split up message into the correct pieces and input
     msg_parts = sms.text.split(',')
-    # if correct num chunks, 3 with date or 2 and use current date
+    size = len(msg_parts)
+    # TODO: Check if first chunk is an actual date
+    if size is 2:
+        current_date = datetime.datetime.today()
+        day_str_item = [str(current_date.month) + '/' + str(current_date.day)]
+        msg_parts = day_str_item + msg_parts
+        print(msg_parts)
+    elif size is 3:
+        pass
+    else:
+        log.debug('Invalid message setup, has %s piece(s)' % str(size))
+        log.debug('Cont: message was-> "%s"' % sms.text)
+        return
+    print(marker)
+    sheet.update_cell(marker.row+1, marker.col, marker.value)
     for i in range(0,3):
         sheet.update_cell(marker.row, marker.col + i, msg_parts[i])
-    # sheet.update_cell(marker.row, marker.col, sms.text)
 
-######### Main #########################
+######### Main ###############################
 
-def update(sms):
-    sheet = get_sheet()
-    # Error check
-    marker = sheet.find("--PREV--")
-    if (sms.from_num == KEVIN):
-        input_message(sms, marker, sheet)
+def handle_sms(sms):
+    sheet = get_sheet(config.sheet_name)
+    if ( sheet != None):
+        try:
+            marker = sheet.find(config.marker)
+        # Dictionary of people and function for each person
+        # Probably more efficient to just pass sheet or something
+        except gspread.exceptions.CellNotFound:
+            log.debug('Unable to find marker')
+        else:
+            # use PEOPLE to check incoming number
+            # check values against sms.from_num
+            input_message(sms, marker, sheet)
     else:
-        print sms.from_num
+        log.debug('Sheet error,  message not input\n "%s"' % sms.text)
